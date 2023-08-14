25f4d4f9ff5bf951c5c2222c9e44bb76a814b696
BenjaminTruckenbrod <btrucker94@gmail.com>
2023-08-13 21:27:25 -0400

    Adding fixes for Zach's comments

--- js/index.js
+++ js/index.js
@@ -2,53 +2,20 @@ document.getElementById("icd-logo").addEventListener("click", gotoICD)
 document.getElementById("icd-name").addEventListener("click", gotoICD)
 document.getElementById("icd-sub").addEventListener("click", goto1420)
 
+function gotoICD() {
+    window.open("https://benjamintruckenbrod.github.io/Ice-Cream-Dreams/", "_blank")
+}
+
+function goto1420() {
+    window.open("https://catalog.upp.pitt.edu/preview_course_nopop.php?coid=1101153&catoid=210", "_blank")
+}
 
 var slugsss = document.getElementsByClassName("slugss-bandcamp");
 for (var i = 0; i < slugsss.length; i++) {
     slugsss[i].addEventListener('click', gotoSlugssBandcamp);
 }
-
 document.getElementById("flyer-text").addEventListener("click", gotoFlyer)
 document.getElementById("flyer-img").addEventListener("click", gotoFlyer)
-
-document.getElementById("search-engine-img").addEventListener("click", gotoSearchEngineVid)
-document.getElementById("search-engine-text").addEventListener("click", gotoSearchEngineVid)
-document.getElementById("INFSCI-1560").addEventListener("click", gotoINFSCI1560)
-
-
-function gotoICD() {
-    // document.location.href = 'https://benjamintruckenbrod.github.io/Ice-Cream-Dreams/';
-    window.open("https://benjamintruckenbrod.github.io/Ice-Cream-Dreams/", "_blank")
-}
-
-function goto1420() {
-    // document.location.href = 'https://catalog.upp.pitt.edu/preview_course_nopop.php?coid=1101153&catoid=210';
-    window.open("https://catalog.upp.pitt.edu/preview_course_nopop.php?coid=1101153&catoid=210", "_blank")
-}
-
-function gotoFigma() {
-    // document.location.href = 'https://www.figma.com/file/u63j9vw0OUTUmVBfF2QS41/Streaming-Tower-of-Pizza-Project?type=design&node-id=430-2&mode=design'
-    window.open("https://www.figma.com/file/u63j9vw0OUTUmVBfF2QS41/Streaming-Tower-of-Pizza-Project?type=design&node-id=430-2&mode=design", "_blank")
-
-}
-
-function gotoSlugssEP() {
-    // document.location.href = 'resources/img/slugss_ep.jpeg'
-    window.open("resources/img/slugss_ep.jpeg", "_blank")
-
-}
-
-function gotoSunSign() {
-    // document.location.href = 'resources/img/sun-sign-ep.jpeg'
-    window.open("resources/img/sun-sign-ep.jpeg", "_blank")
-
-}
-
-function gotoSlugVid() {
-    // document.location.href = 'https://www.instagram.com/p/Bjh4aaegXFg/'
-    window.open("https://www.instagram.com/p/Bjh4aaegXFg/", "_blank")
-
-}
 
 function gotoSlugssBandcamp() {
     // document.location.href = 'https://slugss.bandcamp.com/'
@@ -56,23 +23,15 @@ function gotoSlugssBandcamp() {
 
 };
 
-function gotoCalc() {
-    // document.location.href = 'https://benjamintruckenbrod.github.io/Calculator/'
-    window.open("https://benjamintruckenbrod.github.io/Calculator/", "_blank")
-
-}
-
-function gotoTOP() {
-    // document.location.href = 'https://www.theodinproject.com/lessons/foundations-calculator'
-    window.open("https://www.theodinproject.com/lessons/foundations-calculator", "_blank")
-
-}
-
 function gotoFlyer() {
     // document.location.href = 'resources/img/slugss-fhw-itit.jpeg'
     window.open("resources/img/slugss-fhw-itit.jpeg", "_blank")
 
 }
+
+document.getElementById("search-engine-img").addEventListener("click", gotoSearchEngineVid)
+document.getElementById("search-engine-text").addEventListener("click", gotoSearchEngineVid)
+document.getElementById("INFSCI-1560").addEventListener("click", gotoINFSCI1560)
 
 function gotoSearchEngineVid() {
     // document.location.href = 'resources/video/1560-video.mp4'
